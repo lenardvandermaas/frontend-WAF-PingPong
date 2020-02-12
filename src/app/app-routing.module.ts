3e31e897b8f4a28eb09285f931fb19d884c9393f
Niels Lam <mail@nielslam.nl>
2020-02-12 21:33:00 +0100

    Add routing for matches

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './user-management/login/login.component';
 import {RegisterComponent} from './user-management/register/register.component';
-import { LeaderboardComponent } from './main-app/leaderboard/leaderboard.component';
-import { QueueComponent } from './main-app/queue/queue.component';
-import { StatsComponent } from './main-app/stats/stats.component';
+
+import { MatchListComponent } from './match-list/match-list.component';
+import { MatchDetailsComponent } from './match-details/match-details.component';
+import { MatchConfirmComponent } from './match-confirm/match-confirm.component';
+
+// import { LeaderboardComponent } from './main-app/leaderboard/leaderboard.component';
+// import { QueueComponent } from './main-app/queue/queue.component';
+// import { StatsComponent } from './main-app/stats/stats.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'match', component: RegisterComponent},
+  { path: 'matchlist', component: MatchListComponent},
+  { path: 'matchdetails', component: MatchDetailsComponent},
+  { path: 'matchConfirm', component: MatchConfirmComponent},
   { path: '', redirectTo: '/login', pathMatch: 'full'},
 
   // { path: 'leaderboard', component: LeaderboardComponent},
