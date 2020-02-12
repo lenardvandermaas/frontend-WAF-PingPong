4b96086c0fcae74373257ff8dbb77a7c66231183
Niels Lam <mail@nielslam.nl>
2020-02-12 20:11:59 +0100

    Fix angular poep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './user-management/login/login.component';
+import { RegisterComponent } from './user-management/register/register.component';
 
 
-import { MainMenuComponent } from './main-menu/main-menu.component';
+import { MainMenuComponent } from './main-app/main-menu/main-menu.component';
 import { LeaderStatsQueueComponent } from './leader-stats-queue/leader-stats-queue.component';
 
 @NgModule({
